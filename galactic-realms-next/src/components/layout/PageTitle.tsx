d1cac170f1f7609aae8c50390e3af087c07cee50
Erick Gunawan <erick.gunawan@runchise.com>
2024-07-12 17:20:15 -0600

    fixed commit issues

--- galactic-realms-next/src/components/layout/PageTitle.tsx
+++ galactic-realms-next/src/components/layout/PageTitle.tsx
@@ -6,25 +6,15 @@ type PageTitleProps = {
 };
 export function PageTitle({ children }: PageTitleProps) {
   return (
-<<<<<<< HEAD
     <div className="w-full flex flex-row items-center justify-center p-6">
       <h3
         className={cn(
-          "text-4xl font-bold border-b-2 p-2 w-[30rem] flex flex-row items-center justify-center",
+          "text-7xl font-bold border-b-4 p-6 w-[50rem] flex flex-row items-center justify-center",
           orbitron.className
         )}
       >
         {children}
       </h3>
-=======
-    <div className='w-full flex flex-row items-center justify-center p-6'>
-      <h3 className={cn(
-        'text-7xl font-bold border-b-4 p-6 w-[50rem] flex flex-row items-center justify-center',
-        orbitron.className
-        )}>
-          {children}
-        </h3>
->>>>>>> 8f26b7c65f2deee54923c5dcbbe66488ace94cd0
     </div>
   );
 }
