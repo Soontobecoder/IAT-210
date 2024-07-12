520a59828fbfb47790b8735d85e2c35d6518f58a
Ryan Milligan <ryanmilligan88@gmail.com>
2024-07-12 11:37:48 -0700

    add dummy text for main page components

---
+++ galactic-realms-next/src/app/CoreStatement.tsx
@@ -0,0 +1,13 @@
+
+export function CoreStatement() {
+  return(
+    <div className='w-full'>
+      <h3 className='text-3xl font-orbitron'>
+        Core Statement
+      </h3>
+      <p>
+        Lorem Ipsum doler sit amet.
+      </p>
+    </div>
+  );
+}
