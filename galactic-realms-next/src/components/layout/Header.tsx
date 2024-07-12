af8ec97281c86216086b72f366d21e08e9320595
Ryan Milligan <ryanmilligan88@gmail.com>
2024-07-12 09:47:05 -0700

    add header

--- galactic-realms-next/src/components/layout/Header.tsx
+++ galactic-realms-next/src/components/layout/Header.tsx
@@ -3,8 +3,10 @@
 
 export function Header() {
   return (
-    <header className='w-full absolute h-16 border-b-2'>
-      <h1>Galactic Realms</h1>
+    <header className='w-full absolute px-4 h-20 border-b-2 flex flex-row justify-start items-center'>
+      <h1 className='flex flex-row p-2 h-fit w-fit border-2 text-2xl font-extrabold'>
+        Galactic Realms
+      </h1>
     </header>
   );
 }
