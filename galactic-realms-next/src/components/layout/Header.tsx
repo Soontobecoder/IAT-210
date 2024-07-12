4a38513bcd6be059550476bd10a860781c03871f
Ryan Milligan <ryanmilligan88@gmail.com>
2024-07-12 11:35:09 -0700

    add Navbar

--- galactic-realms-next/src/components/layout/Header.tsx
+++ galactic-realms-next/src/components/layout/Header.tsx
@@ -1,14 +1,38 @@
 import { orbitron } from '@/fonts';
 import { cn } from '@/utils/cn';
+import { Navbar } from './Navbar';
+import Link from 'next/link';
 
 
 
 export function Header() {
   return (
-    <header className={cn('w-full absolute px-4 h-20 border-b-2 flex flex-row justify-start items-center', orbitron.className)}>
-      <h1 className={cn('flex flex-row p-2 h-fit w-fit border-2 text-2xl font-bold')}>
-        Galactic Realms
-      </h1>
+    <header className={cn(
+      'w-full absolute px-8 h-20 border-b-2 flex flex-row justify-start items-center', 
+      'gap-4',
+      orbitron.className,
+      )}>
+
+      <div className={cn(
+        'w-full h-full flex flex-row items-center justify-start',
+        // 'border-2 border-red-500',
+      )}>
+        <h1 className={cn(
+          'flex flex-row p-2 h-fit w-fit border-2 text-2xl font-bold mx-4',
+          'hover:text-indigo-600 transition-all'
+          )}>
+            <Link href={'/'}>
+              Galactic Realms
+            </Link>
+        </h1>
+      </div>
+
+      <div className={cn(
+        'w-full h-full flex flex-row items-center justify-center',
+        // 'border-2 border-red-500',
+      )}>
+        <Navbar />
+      </div>
     </header>
   );
 }
---
+++ galactic-realms-next/src/components/layout/Navbar.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link'
+
+import { orbitron } from '@/fonts';
+import { cn } from '@/utils/cn';
+
+
+type NavLinkProps = {
+  label: string,
+  href: string
+}
+
+const links: NavLinkProps[] = [
+  {
+    label: 'Inspiration',
+    href: '/inspiration',
+  },
+  {
+    label: 'Logs',
+    href: '/logs',
+  },
+  {
+    label: 'Rules',
+    href: '/rules',
+  },
+  {
+    label: 'Gallery',
+    href: '/gallery',
+  },
+  {
+    label: 'References',
+    href: '/references',
+  },
+  // {
+  //   label: '',
+  //   href: '/',
+  // },
+  // {
+  //   label: '',
+  //   href: '/',
+  // },
+  // {
+  //   label: '',
+  //   href: '/',
+  // },
+];
+
+export function Navbar() {
+  return (
+    <div className={cn('w-fit mx-4 p-2 border-2 flex flex-row justify-start items-center', orbitron.className)}>
+      {links.map((link) => (<NavLink key={link.href} {...link}/>))}
+    </div>
+  );
+}
+
+function NavLink({label, href}: NavLinkProps) {
+  return(
+  
+    <Link 
+      className='p-2 hover:text-indigo-600 transition-all'
+      href={href}
+    >
+      {label}
+    </Link>
+  );
+}
