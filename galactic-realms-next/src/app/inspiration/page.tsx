3e2f1814c486aa45d63ed3b32ccd4b6006f42f47
Erick Gunawan <erick.gunawan@runchise.com>
2024-07-12 17:17:29 -0600

    added inspiration page layout

--- galactic-realms-next/src/app/inspiration/page.tsx
+++ galactic-realms-next/src/app/inspiration/page.tsx
@@ -2,10 +2,6 @@ import { Suspense } from "react";
 import { cn } from "@/utils/cn";
 import { orbitron } from "@/fonts";
 
-import { Banner } from "./Banner";
-import Card from "./Card";
-import { Spinner } from "@radix-ui/themes";
-
 export default function InspirationPage() {
   return (
     <main
@@ -20,7 +16,7 @@ export default function InspirationPage() {
           Inspiration
         </h2>
       </div>
-      <div className="grid grid-flow-col grid-cols-6">
+      <div className="grid grid-flow-col grid-cols-6 mb-10">
         <div className="col-start-2 col-span-4 mt-10">
           <p style={{ textAlign: "justify" }}>
             It is a long established fact that a reader will be distracted by
@@ -34,6 +30,97 @@ export default function InspirationPage() {
             years, sometimes by accident, sometimes on purpose (injected humour
             and the like).
           </p>
+        </div>
+      </div>
+
+      {/* Carcassone */}
+      <div className="grid grid-cols-6 grid-flow-col gap-2 mt-10 mb-10">
+        <div
+          className="col-start-2 col-span-2 row-span-3 mr-5"
+          style={{ borderRight: "solid", paddingRight: "20px" }}
+        >
+          <img
+            style={{ width: "400px" }}
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Carcassonne-game.jpg/220px-Carcassonne-game.jpg"
+          />
+        </div>
+        <div className="col-span-2 ...">
+          <h2 className={cn("relative text-4xl", orbitron.className)}>
+            Carcassonne
+          </h2>
+        </div>
+        <div className="row-span-2 col-span-2 ...">
+          <p style={{ textAlign: "justify" }}>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </p>
+          <p className="mt-5" style={{ textAlign: "justify" }}>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </p>
+        </div>
+      </div>
+
+      {/* Second inspiration */}
+      <div className="grid grid-cols-6 grid-flow-col gap-2 mt-12">
+        <div className="col-span-2 col-start-2">
+          <h2 className={cn("relative text-4xl", orbitron.className)}>
+            Carcassonne
+          </h2>
+        </div>
+        <div className="row-span-2 col-start-2 col-span-2 ...">
+          <p style={{ textAlign: "justify" }}>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </p>
+          <p className="mt-5" style={{ textAlign: "justify" }}>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </p>
+        </div>
+        <div
+          className="col-span-2 row-span-3 ml-5 flex items-end"
+          style={{
+            borderLeft: "solid",
+            paddingLeft: "20px",
+          }}
+        >
+          <img
+            style={{ width: "400px", alignItems: "end" }}
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Carcassonne-game.jpg/220px-Carcassonne-game.jpg"
+          />
         </div>
       </div>
     </main>
--- galactic-realms-next/src/components/layout/PageTitle.tsx
+++ galactic-realms-next/src/components/layout/PageTitle.tsx
@@ -1,19 +1,20 @@
-import { orbitron } from '@/fonts'
-import { cn } from '@/utils/cn'
-
+import { orbitron } from "@/fonts";
+import { cn } from "@/utils/cn";
 
 type PageTitleProps = {
-  children: string,
-}
-export function PageTitle({children}: PageTitleProps) {
+  children: string;
+};
+export function PageTitle({ children }: PageTitleProps) {
   return (
-    <div className='w-full flex flex-row items-center justify-center p-6'>
-      <h3 className={cn(
-        'text-4xl font-bold border-b-2 p-2 w-[30rem] flex flex-row items-center justify-center',
-        orbitron.className
-        )}>
-          {children}
-        </h3>
+    <div className="w-full flex flex-row items-center justify-center p-6">
+      <h3
+        className={cn(
+          "text-4xl font-bold border-b-2 p-2 w-[30rem] flex flex-row items-center justify-center",
+          orbitron.className
+        )}
+      >
+        {children}
+      </h3>
     </div>
-  )
+  );
 }
