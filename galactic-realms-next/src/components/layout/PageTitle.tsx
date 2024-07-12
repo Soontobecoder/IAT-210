763655344543511d276f41bc564549e1efe9e713
Ryan Milligan <ryanmilligan88@gmail.com>
2024-07-12 15:50:09 -0700

    increase PageTitle font size

--- galactic-realms-next/src/components/layout/PageTitle.tsx
+++ galactic-realms-next/src/components/layout/PageTitle.tsx
@@ -9,7 +9,7 @@ export function PageTitle({children}: PageTitleProps) {
   return (
     <div className='w-full flex flex-row items-center justify-center p-6'>
       <h3 className={cn(
-        'text-4xl font-bold border-b-2 p-2 w-[30rem] flex flex-row items-center justify-center',
+        'text-7xl font-bold border-b-4 p-6 w-[50rem] flex flex-row items-center justify-center',
         orbitron.className
         )}>
           {children}
