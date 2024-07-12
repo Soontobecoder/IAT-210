f217d4a2d0f1d0fbd66445b846ce8b7a82bb92ec
Ryan Milligan <ryanmilligan88@gmail.com>
2024-07-12 11:39:58 -0700

    add suspense spinner

--- galactic-realms-next/src/app/page.tsx
+++ galactic-realms-next/src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from 'react'
 
 import { Banner } from './Banner';
+import { Spinner } from '@radix-ui/themes';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24">
       
-      <Suspense fallback={<p>Loading feed...</p>}>
+      <Suspense fallback={<Spinner/>}>
         <Banner />
       </Suspense>
       
