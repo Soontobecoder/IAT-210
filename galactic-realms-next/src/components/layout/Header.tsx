c1bd510f7f36cc0099ecef0b6d0320fe6fd9ec72
Ryan Milligan <ryanmilligan88@gmail.com>
2024-07-12 11:12:29 -0700

    fix banner

--- galactic-realms-next/src/components/layout/Header.tsx
+++ galactic-realms-next/src/components/layout/Header.tsx
@@ -5,8 +5,8 @@ import { cn } from '@/utils/cn';
 
 export function Header() {
   return (
-    <header className='w-full absolute px-4 h-20 border-b-2 flex flex-row justify-start items-center'>
-      <h1 className={cn('flex flex-row p-2 h-fit w-fit border-2 text-2xl font-extrabold font-orbitron', orbitron.className)}>
+    <header className={cn('w-full absolute px-4 h-20 border-b-2 flex flex-row justify-start items-center', orbitron.className)}>
+      <h1 className={cn('flex flex-row p-2 h-fit w-fit border-2 text-2xl font-bold')}>
         Galactic Realms
       </h1>
     </header>
