8df3d412d6bd13af38ba326d12ea09769add9a41
Erick Gunawan <erick.gunawan@runchise.com>
2024-07-12 23:38:48 -0600

    made the navbar sticky

--- galactic-realms-next/src/components/layout/Navbar.tsx
+++ galactic-realms-next/src/components/layout/Navbar.tsx
@@ -1,34 +1,33 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-import { orbitron } from '@/fonts';
-import { cn } from '@/utils/cn';
-
+import { orbitron } from "@/fonts";
+import { cn } from "@/utils/cn";
 
 type NavLinkProps = {
-  label: string,
-  href: string
-}
+  label: string;
+  href: string;
+};
 
 const links: NavLinkProps[] = [
   {
-    label: 'Inspiration',
-    href: '/inspiration',
+    label: "Inspiration",
+    href: "/inspiration",
   },
   {
-    label: 'Logs',
-    href: '/logs',
+    label: "Logs",
+    href: "/logs",
   },
   {
-    label: 'Rules',
-    href: '/rules',
+    label: "Rules",
+    href: "/rules",
   },
   {
-    label: 'Gallery',
-    href: '/gallery',
+    label: "Gallery",
+    href: "/gallery",
   },
   {
-    label: 'Flowchart',
-    href: '/flowchart',
+    label: "Flowchart",
+    href: "/flowchart",
   },
   // {
   //   label: '',
@@ -39,26 +38,29 @@ const links: NavLinkProps[] = [
   //   href: '/',
   // },
   {
-    label: 'References',
-    href: '/references',
+    label: "References",
+    href: "/references",
   },
 ];
 
 export function Navbar() {
   return (
-    <div className={cn('w-fit mx-4 p-2 border-2 flex flex-row justify-start items-center', orbitron.className)}>
-      {links.map((link) => (<NavLink key={link.href} {...link}/>))}
+    <div
+      className={cn(
+        "w-fit mx-4 p-2 border-2 flex flex-row justify-start items-center",
+        orbitron.className
+      )}
+    >
+      {links.map((link) => (
+        <NavLink key={link.href} {...link} />
+      ))}
     </div>
   );
 }
 
-function NavLink({label, href}: NavLinkProps) {
-  return(
-  
-    <Link 
-      className='p-2 hover:text-indigo-600 transition-all'
-      href={href}
-    >
+function NavLink({ label, href }: NavLinkProps) {
+  return (
+    <Link className="p-2 hover:text-indigo-600 transition-all" href={href}>
       {label}
     </Link>
   );
