35538285e7bdf9938c8377c9297421d89246a116
Ryan Milligan <ryanmilligan88@gmail.com>
2024-07-14 13:09:46 -0700

    add flowchart to page

--- galactic-realms-next/src/app/flowchart/page.tsx
+++ galactic-realms-next/src/app/flowchart/page.tsx
@@ -1,6 +1,11 @@
+
+import Image from "next/image";
+
 import { PageTitle } from "@/components/layout/PageTitle";
 import { orbitron } from "@/fonts";
 import { cn } from "@/utils/cn";
+
+import flowchart from '/public/flowchart/flowchart.jpg'
 
 export default function FlowchartPage() {
   return (
@@ -9,6 +14,10 @@ export default function FlowchartPage() {
       className="flex min-h-screen flex-col items-center justify-start p-24"
     >
       <PageTitle>Flowchart</PageTitle>
+
+
+      <Image src={flowchart} alt="Flowchart" priority />
+
     </main>
   );
 }
