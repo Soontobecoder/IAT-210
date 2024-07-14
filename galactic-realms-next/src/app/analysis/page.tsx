9221456c4aaa2e12c5d420e24362f6fc118e1c86
Ryan Milligan <ryanmilligan88@gmail.com>
2024-07-14 12:02:04 -0700

    add contributions breakdown

---
+++ galactic-realms-next/src/app/analysis/page.tsx
@@ -0,0 +1,14 @@
+import { PageTitle } from "@/components/layout/PageTitle";
+import { orbitron } from "@/fonts";
+import { cn } from "@/utils/cn";
+
+export default function AnalysisPage() {
+  return (
+    <main
+      style={{ marginLeft: "80px", marginRight: "80px" }}
+      className="flex min-h-screen flex-col items-center justify-start p-24"
+    >
+      <PageTitle>Analysis</PageTitle>
+    </main>
+  );
+}
